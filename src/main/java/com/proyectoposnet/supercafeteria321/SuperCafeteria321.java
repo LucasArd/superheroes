dc5defe76c34d24486c4c8087306583c3fe8eac6
LucasArd <luqasaranda@outlook.com>
2024-10-07 19:20:23 -0300

    cafeterias321

---
+++ src/main/java/com/proyectoposnet/supercafeteria321/SuperCafeteria321.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyectoposnet.supercafeteria321;
+
+/**
+ *
+ * @author luqas
+ */
+public class SuperCafeteria321 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
